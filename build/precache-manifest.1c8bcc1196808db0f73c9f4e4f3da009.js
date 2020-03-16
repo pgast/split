ad459b49f549767562689e9b9d4015b8717cdfdc
Pablo Gastelum Leyva <Pablo@Pablo-2.local>
2020-03-15 18:15:59 -0700

    fix pwa config issues

---
+++ build/precache-manifest.1c8bcc1196808db0f73c9f4e4f3da009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a71b745803e2a2cf066e09dc7cf7b6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72d43efa237062702cea",
+    "url": "/static/css/main.9cb0c538.chunk.css"
+  },
+  {
+    "revision": "76bdf56e47aa6cc58ec6",
+    "url": "/static/js/2.779365cd.chunk.js"
+  },
+  {
+    "revision": "497d7e07705261348096eb742962e0f0",
+    "url": "/static/js/2.779365cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72d43efa237062702cea",
+    "url": "/static/js/main.02c7dc5b.chunk.js"
+  },
+  {
+    "revision": "52cc4e5e449165d82f38",
+    "url": "/static/js/runtime-main.cef296f8.js"
+  }
+]);
